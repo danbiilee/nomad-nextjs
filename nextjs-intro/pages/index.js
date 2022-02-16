4496645980002faa3088e426905b18fdd4c425b3
danbiilee <danbi.db@gmail.com>
2022-02-16 20:37:28 +0900

    redirect, rewrite, ssr

--- nextjs-intro/pages/index.js
+++ nextjs-intro/pages/index.js
@@ -1,31 +1,50 @@
-import { useEffect, useState } from 'react';
-import Seo from './Seo';
+import Seo from '../components/Seo';
 
-const API_KEY = '5927cf0e621ff608b28c9448001ca9a4';
-
-export default function Home() {
-  const [movies, setMovies] = useState();
-
-  useEffect(() => {
-    (async () => {
-      const { results } = await (
-        await fetch(
-          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
-        )
-      ).json();
-      setMovies(results);
-    })();
-  }, []);
-  console.log(movies);
+export default function Home({ results }) {
   return (
-    <div>
-      <Seo title="HOME" />
-      {!movies && <h4>Loading...</h4>}
-      {movies?.map((movie) => (
-        <div key={movie.id}>
+    <div className="container">
+      <Seo title="Home" />
+      {results?.map((movie) => (
+        <div className="movie" key={movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
         </div>
       ))}
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie {
+          cursor: pointer;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch(`http://localhost:3000/api/movies`)
+  ).json();
+  return {
+    props: {
+      results: JSON.parse(JSON.stringify(results)),
+    },
+  };
 }
